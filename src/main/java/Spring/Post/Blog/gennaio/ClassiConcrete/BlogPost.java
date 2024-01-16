4130d802d8041ec49e311a8c55ee7ca43b2a2980
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-01-16 13:55:19 +0100

    basi springWeb

---
+++ src/main/java/Spring/Post/Blog/gennaio/ClassiConcrete/BlogPost.java
@@ -0,0 +1,12 @@
+package Spring.Post.Blog.gennaio.ClassiConcrete;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BlogPost {
+    
+}
